4c428edf92a37b36e6dedd6cd9c8cb877cd6b533
Matthew Huxtable <git@tigermatt.uk>
2022-12-10 12:14:11 +0000

    day 10: part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -62,24 +62,35 @@ fn parse(input: &str) -> Result<Vec<Instruction>, ParseInstructionError> {
     input.lines().map(|l| l.parse()).collect()
 }
 
+const CRT_SIZE: usize = 240;
+const CRT_ROWS: usize = 6;
+const CRT_WIDTH: usize = CRT_SIZE / CRT_ROWS;
+const SPRITE_WIDTH: usize = 3;
+
 struct CPU<I: IntoIterator> {
     instrs: I::IntoIter,
+    cycle: usize,
     reg_x: isize,
     current_instruction: Option<Instruction>,
     remaining_ticks: usize,
+    crt: Vec<bool>,
 }
 
 impl<I: IntoIterator<Item = Instruction>> CPU<I> {
     pub fn new(instructions: I) -> CPU<I> {
         CPU {
             instrs: instructions.into_iter(),
+            cycle: 0,
             reg_x: 1,
             current_instruction: None,
             remaining_ticks: 0,
+            crt: vec![false; CRT_SIZE],
         }
     }
 
     pub fn tick(&mut self) -> bool {
+        self.cycle += 1;
+
         if self.remaining_ticks == 0 {
             self.current_instruction = self.instrs.next().and_then(|i| {
                 self.remaining_ticks = i.ticks();
@@ -91,8 +102,17 @@ impl<I: IntoIterator<Item = Instruction>> CPU<I> {
             return false;
         }
 
-        let i = self.current_instruction.as_ref().unwrap();
+        // Pixels are numbered 0 to 39, etc. but cycle will be 1 to 40 for the first row.
+        let current_pixel = ((self.cycle - 1) % CRT_WIDTH) as isize;
+        // The sprite is SPRITE_WIDTH wide, which means it protrudes (SPRITE_WIDTH-1)/2 to the left
+        // and right relative to the value of the register that determines its middle.
+        let visible = (self.reg_x - (SPRITE_WIDTH as isize - 1) / 2) <= current_pixel
+            && (self.reg_x + (SPRITE_WIDTH as isize - 1) / 2) >= current_pixel;
+        // The inputs don't seem to wrap the CPU state beyond cycle CRT_SIZE, but in principle they
+        // could wrap and require us to overwrite previous output frame state.
+        self.crt[(self.cycle - 1) % CRT_SIZE] = visible;
 
+        let i = self.current_instruction.as_ref().unwrap();
         match i {
             Instruction::Noop => {}
             Instruction::Addx(x) => {
@@ -106,37 +126,63 @@ impl<I: IntoIterator<Item = Instruction>> CPU<I> {
         return true;
     }
 
+    pub fn get_cycle(&self) -> usize {
+        self.cycle
+    }
+
     pub fn get_x(&self) -> isize {
         self.reg_x
     }
-}
 
-fn run_scores<I: IntoIterator<Item = Instruction>>(cpu: &mut CPU<I>) -> i32 {
-    let mut score = 0;
+    pub fn get_crt(&self) -> String {
+        let mut display = String::new();
 
-    // Cycles don't start counting at 0!
-    for cycle in 1..=220 {
-        // Signal strength is computed before the CPU ticks
-        if cycle % 40 == 20 {
-            score += cpu.get_x() as i32 * cycle;
+        for (i, &pixel) in self.crt.iter().enumerate() {
+            if i > 0 && i % (CRT_SIZE / CRT_ROWS) == 0 {
+                display.push('\n');
+            }
+
+            display.push(if pixel { '#' } else { '.' });
         }
 
-        if !cpu.tick() {
-            panic!("ran out of instructions");
-        }
+        display
     }
-
-    score
 }
 
 pub fn part_one(input: &str) -> Option<i32> {
     let instructions = parse(input).expect("error parsing input");
     let mut cpu = CPU::new(instructions);
-    Some(run_scores(&mut cpu))
+    let mut score = 0;
+
+    loop {
+        // Cycles start at 1, the CPU will only update cycle after the first tick, but signal
+        // strength is computed from before the tick applies to the state.
+        let cycle = cpu.get_cycle() + 1;
+
+        if cycle % 40 == 20 {
+            score += cpu.get_x() as i32 * cycle as i32;
+        }
+
+        if !cpu.tick() {
+            if cycle < 220 {
+                panic!("ran out of instructions!");
+            } else {
+                break;
+            }
+        }
+    }
+
+    Some(score)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let instructions = parse(input).expect("error parsing input");
+    let mut cpu = CPU::new(instructions);
+    while cpu.tick() {}
+    let crt = cpu.get_crt();
+    // println!("{}", crt);
+
+    Some(crt)
 }
 
 fn main() {
@@ -144,6 +190,13 @@ fn main() {
     advent_of_code::solve!(1, part_one, input);
     advent_of_code::solve!(2, part_two, input);
 }
+
+const PART_TWO: &str = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....";
 
 #[cfg(test)]
 mod tests {
@@ -158,6 +211,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 10);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(PART_TWO.to_string()));
     }
 }
