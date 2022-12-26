174c9b55f170718d7d1c72d7d78b72b8616bd5b6
Matthew Huxtable <git@tigermatt.uk>
2022-12-26 18:20:03 +0000

    day 24: part 2

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -81,22 +81,32 @@ fn parse(
     (result, (width, height))
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (mut blizzards, (width, height)) = parse(input);
-    let mut reachability: HashMap<(isize, isize), Vec<usize>> = HashMap::new();
+struct Puzzle {
+    blizzards: HashMap<(isize, isize), Vec<BlizzardDirection>>,
+    dimensions: (usize, usize), // width x height
+}
 
-    // We can reach the starting location at step 0.
-    reachability.insert((0, -1), vec![0]);
+#[derive(Clone, Copy, Debug)]
+enum ValleyPortal {
+    TopLeft,
+    BottomRight,
+}
 
-    let mut steps = 0;
+impl ValleyPortal {
+    fn other(&self) -> Self {
+        match self {
+            Self::TopLeft => Self::BottomRight,
+            Self::BottomRight => Self::TopLeft,
+        }
+    }
+}
 
-    loop {
-        steps += 1;
-
+impl Puzzle {
+    pub fn step_blizzards(&mut self) {
         // Update positions of all blizzards
-        blizzards = {
+        self.blizzards = {
             let mut next = HashMap::new();
-            let resolve = |cur, max| {
+            fn resolve_dimension(cur: isize, max: isize) -> isize {
                 if cur < 0 {
                     max - 1
                 } else if cur >= max {
@@ -104,20 +114,36 @@ pub fn part_one(input: &str) -> Option<u32> {
                 } else {
                     cur
                 }
+            }
+
+            let resolve = |(x, y)| {
+                // blizzards in part 2 need to account for going to the entrances and exits
+                // :scream:
+                if (x == 0 && y == -1)
+                    || (x == self.dimensions.0 as isize - 1 && y == self.dimensions.1 as isize)
+                {
+                    (x, y)
+                } else if x == 0 && y == self.dimensions.1 as isize {
+                    // The blizzard at the left will start from the entrance, not y = 0 (we don't
+                    // need to special case the exit blizzard wrapping as that will just start
+                    // again at y = 0 in the else case.
+                    (0, -1)
+                } else {
+                    (
+                        resolve_dimension(x, self.dimensions.0 as isize),
+                        resolve_dimension(y, self.dimensions.1 as isize),
+                    )
+                }
             };
 
-            for ((x, y), directions) in blizzards.clone() {
+            for ((x, y), directions) in self.blizzards.clone() {
                 for direction in directions {
-                    next.entry({
-                        let (x, y) = match direction {
-                            BlizzardDirection::Up => (x, y - 1),
-                            BlizzardDirection::Down => (x, y + 1),
-                            BlizzardDirection::Left => (x - 1, y),
-                            BlizzardDirection::Right => (x + 1, y),
-                        };
-
-                        (resolve(x, width as isize), resolve(y, height as isize))
-                    })
+                    next.entry(resolve(match direction {
+                        BlizzardDirection::Up => (x, y - 1),
+                        BlizzardDirection::Down => (x, y + 1),
+                        BlizzardDirection::Left => (x - 1, y),
+                        BlizzardDirection::Right => (x + 1, y),
+                    }))
                     .or_insert(vec![])
                     .push(direction);
                 }
@@ -125,16 +151,37 @@ pub fn part_one(input: &str) -> Option<u32> {
 
             next
         };
+    }
 
-        // Update reachability for all positions that we could reach in the last position and which
-        // do not currently have a blizzard occupying them or the adjacent.
-        {
-            for row in 0..height {
-                for col in 0..width {
+    pub fn solve(&mut self, start: ValleyPortal) -> Option<u32> {
+        let mut reachability: HashMap<(isize, isize), Vec<usize>> = HashMap::new();
+        let (width, height) = self.dimensions;
+
+        // We can reach the starting location at step 0.
+        reachability.insert(
+            match start {
+                ValleyPortal::TopLeft => (0, -1),
+                ValleyPortal::BottomRight => (width as isize - 1, height as isize),
+            },
+            vec![0],
+        );
+
+        let mut steps = 0;
+
+        loop {
+            eprintln!("solve has stepped {} times", steps);
+            steps += 1;
+
+            self.step_blizzards();
+
+            // Update reachability for all positions that we could reach in the last position and which
+            // do not currently have a blizzard occupying them or the adjacent.
+            for row in -1..height as isize + 1 {
+                for col in 0..width as isize {
                     let (row, col) = (row as isize, col as isize);
 
-                    // if there's a blizzard here, we can't stay
-                    if !blizzards.entry((col, row)).or_default().is_empty() {
+                    if self.blizzards.entry((col, row)).or_default().len() > 0 {
+                        // there's a blizzard here, we can't stay
                         continue;
                     }
 
@@ -150,15 +197,18 @@ pub fn part_one(input: &str) -> Option<u32> {
                         .iter()
                         // remove points outside the grid that cannot be accessed
                         .filter(|(x, y)| {
-                            // The final position is outside the grid
-                            (*x == width as isize - 1 && *y == height as isize + 1)
-                                || (*x >= 0
-                                    && *x < width as isize
-                                    && *y >= -1
-                                    && *y < height as isize)
+                            // The only position permitted outside the grid is the valley entrances
+                            // and exists, which are modelled at (0,-1) and (width-1, height)
+                            (*x == 0 && *y == -1) // start
+                                    || (*x == width as isize - 1 && *y == height as isize) // end
+                                    || (*x >= 0
+                                        && *x < width as isize
+                                        && *y >= 0
+                                        && *y < height as isize)
                         })
                         // to access the point we have to have visited at least one of the adjacent
-                        // candidates in the previous round
+                        // candidates in the previous round or be at the current point and have
+                        // stayed here (assuming we can)
                         .any(|(x, y)| {
                             reachability
                                 .entry((*x, *y))
@@ -171,23 +221,66 @@ pub fn part_one(input: &str) -> Option<u32> {
                     }
                 }
             }
+
+            // Check if the end was reached
+            if reachability
+                .entry(
+                    // We're going to the opposite side to where we started.
+                    match start {
+                        ValleyPortal::TopLeft => (width as isize - 1, height as isize),
+                        ValleyPortal::BottomRight => (0, -1),
+                    },
+                )
+                .or_default()
+                .len()
+                > 0
+            {
+                break;
+            }
         }
 
-        // Check if the end was reached
-        if !reachability
-            .entry((width as isize - 1, height as isize - 1))
-            .or_default()
-            .is_empty()
-        {
-            break;
-        }
+        Some(steps as u32)
     }
+}
 
-    Some(steps as u32 + 1)
+pub fn part_one(input: &str) -> Option<u32> {
+    let (blizzards, (width, height)) = parse(input);
+
+    let result = Puzzle {
+        blizzards,
+        dimensions: (width, height),
+    }
+    .solve(ValleyPortal::TopLeft);
+
+    Some(result.unwrap())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (blizzards, dimensions) = parse(input);
+
+    let mut steps = 0;
+    let mut puzzle = Puzzle {
+        blizzards,
+        dimensions,
+    };
+
+    let mut start = ValleyPortal::TopLeft;
+
+    // 1. there
+    // 2. back
+    // 3. there again
+    for step in 0..3 {
+        let this_pass = puzzle.solve(start).unwrap();
+        steps += this_pass;
+        start = start.other();
+
+        println!(
+            "solved step {} in {} minutes (for {} total)",
+            step, this_pass, steps
+        );
+    }
+
+    Some(steps)
 }
 
 fn main() {
@@ -209,6 +302,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 24);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(54));
     }
 }
